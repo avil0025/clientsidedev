0727f2dd3a662526fb0add0c13016e4f41566919
avil0025 <avil0025@algonquinlive.com>
2025-05-20 22:11:11 -0400

    Create index.js

---
+++ printingoutput/indexjs/index.js
@@ -0,0 +1,43 @@
+// ACTIVITY 1____________________________________________________________________________
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = `
+let a = 3;
+let b = 5;
+let c;
+---------------
+a+b = ${a + b}
+a-b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a + -b}
+a -= b = ${(a -= b)}
+a *= b = ${(a *= b)}
+a /= b = ${(a /= b)}
+a %= b = ${(a %= b)}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+`;
+
+console.log(output);
+
+
+
+// ACTIVITY 2____________________________________________________________________________
+
+
+let first_name = "Laura";
+let last_name = "Avila";
+let email = "laura.avila@mail.com";
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+console.log(output);
