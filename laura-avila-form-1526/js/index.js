f66a296678e09ccabed83b22a8e1a916b665f769
avil0025 <avil0025@algonquinlive.com>
2025-07-01 04:09:44 -0400

    Update index.js

--- laura-avila-form-1526/js/index.js
+++ laura-avila-form-1526/js/index.js
@@ -1,4 +1,3 @@
-// access the form and inputs
 let fm = document.getElementById("my-form");
 let fn = document.getElementById("fname");
 let em = document.getElementById("email");
@@ -6,21 +5,16 @@ let msg = document.getElementById("msg");
 let btn = document.getElementById("submit-btn");
 let err = document.getElementById("err");
 
-// define event-handler
 function handleForm() {
   console.log(fn.value);
   let ui = {};
   let errors = [];
-  // define containers for user-input/errors
 
-  // validate full name:
   if (fn.value.length > 0) {
-    // save that value in ui
     ui = {
       name: fn,
     };
   } else {
-    // create corresponding error message and push it in the errors array
     errors.push("The name is required!");
   }
 
